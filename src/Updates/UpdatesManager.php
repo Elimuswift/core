b76122674de7ba83be3ad7d713d6375fe0485211
Master Weez <wizqydy@gmail.com>
2017-03-22 18:35:26 +0300

    switch to PSR2 codding style

--- src/Updates/UpdatesManager.php
+++ src/Updates/UpdatesManager.php
@@ -1,49 +1,54 @@
 <?php
+
 namespace Elimuswift\Core\Updates;
 
 use Elimuswift\Core\Update;
 use Elimuswift\Core\Repositories\Contracts\RepositoryContract;
 
 /**
-* Manage application updates
-*/
+ * Manage application updates.
+ */
 class UpdatesManager
 {
-	/**
-	 * Update Repository 
-	 *
-	 * @var mixed
-	 **/
-	public $repository;
-	/**
-	 * Create a new instance of the UpdatesManager::class
-	 * 
-	 **/
-	
-	public function __construct(RepositoryContract $repository)
-	{
-		$this->repository = $repository;
-	}
+    /**
+     * Update Repository.
+     *
+     * @var mixed
+     **/
+    public $repository;
 
-	/**
-	 * Determin if the customer has already verified the product
-	 *
-	 * @return void
-	 * @param string $purchaseKey 
-	 **/
-	public function verifyCustomer($purchaseKey)
-	{
-		$this->repository->findCustomer($purchaseKey);
-	}
+    /**
+     * Create a new instance of the UpdatesManager::class.
+     *
+     * @param RepositoryContract $repository description
+     **/
+    public function __construct(RepositoryContract $repository)
+    {
+        $this->repository = $repository;
+    }
 
-	/**
-	 * Get a specific update version
-	 *
-	 * @return void
-	 * @param string $version Build version 
-	 **/
-	public function getRelease($version)
-	{
-	}
+//end __construct()
 
-}
+    /**
+     * Determin if the customer has already verified the product.
+     *
+     * @param string $purchaseKey
+     **/
+    public function verifyCustomer($purchaseKey)
+    {
+        $this->repository->findCustomer($purchaseKey);
+    }
+
+//end verifyCustomer()
+
+    /**
+     * Get a specific update version.
+     *
+     * @param string $version Build version
+     **/
+    public function getRelease($version)
+    {
+    }
+
+//end getRelease()
+}//end class
